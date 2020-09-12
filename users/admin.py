14a389caa5790f40dde40fc093c5326653b62536
Leo Kukharau <kuhorev.leonid@gmail.com>
2020-09-12 15:24:39 -0700

    cutom fields added to the admin site form

--- users/admin.py
+++ users/admin.py
@@ -2,15 +2,24 @@ from django.contrib import admin
 from django.contrib.auth import get_user_model
 from django.contrib.auth.admin import UserAdmin
 
-from .forms import UserRegisterForm, UserCreationForm
+from .forms import UserRegisterForm, UserUpdateForm
 from .models import CustomUser
 
 
 class CustomUserAdmin(UserAdmin):
-    add_form = UserCreationForm
-    form = UserRegisterForm
+    add_form = UserRegisterForm
+    form = UserUpdateForm
     model = CustomUser
     list_display = ['email', 'username']
 
+    # CREDIT: https://stackoverflow.com/questions/28897480/django-admin-custom-create-user-form
+    prepopulated_fields = {'username': ('first_name' , 'last_name', )}
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'first_name',
+                  'last_name', 'username', 'password1', 'password2' ),
+        }),
+    )
 
 admin.site.register(CustomUser, CustomUserAdmin)
